3b8adf2d6cbd4b9b7c63d9c0b7dcc81a6ea4932a
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-12-24 20:15:01 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size):
+        """ Private instance attribute """
+        self.__size = size
+
+
+if __name__ == "__main__":
+    Square()
